6ecbb869df93ea077705265d7a39294138fbe0ce
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-11-11 02:48:10 +1100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.js",
+  },
+  base: "/",
+  server: {
+    host: true,
+  },
+  plugins: [react()],
+})
