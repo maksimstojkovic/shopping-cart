eac23c1bfca15f7781f78295e0859c58ed9b99b6
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-11-12 23:45:13 +1100

    Update checkout page

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,5 +1,10 @@
 const Checkout = () => {
-  return <h2>Checkout</h2>;
+  return (
+    <>
+      <h2>Checkout</h2>
+      <p>Logic not implemented as part of project</p>
+    </>
+  );
 };
 
 export default Checkout;
