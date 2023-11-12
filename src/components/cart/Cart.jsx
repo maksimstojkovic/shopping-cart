4e412ad685c9159c3455b48dcd4882e61a751495
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-11-13 01:37:59 +1100

    Add classes to cart header and cards

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -6,16 +6,18 @@ const Cart = () => {
 
   return (
     <>
-      <header>
+      <header className="split">
         <h2>Shopping Cart</h2>
         <Link to={import.meta.env.BASE_URL + "checkout"}>
           <button type="button">Checkout</button>
         </Link>
       </header>
 
+      <div className="cart-cards">
       {cart.map((item) => (
         <CartCard key={item.id} id={item.id} cartState={[cart, setCart]} />
       ))}
+      </div>
     </>
   );
 };
